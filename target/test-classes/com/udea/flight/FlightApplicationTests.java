e59f54b751121f48cdaf6b1580076ed94022c328
Mgll3 <miguel.alvarez1@udea.edu.co>
2024-04-20 13:19:24 -0500

    MYSQL

---
+++ target/test-classes/com/udea/flight/FlightApplicationTests.java
@@ -0,0 +1,21 @@
+/*
+ * Decompiled with CFR 0.152.
+ * 
+ * Could not load the following classes:
+ *  org.junit.jupiter.api.Test
+ *  org.springframework.boot.test.context.SpringBootTest
+ */
+package com.udea.flight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightApplicationTests {
+    FlightApplicationTests() {
+    }
+
+    @Test
+    void contextLoads() {
+    }
+}
